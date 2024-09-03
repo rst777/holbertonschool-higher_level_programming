cc4f0e691ffba65f605c30aef10c3e24aa15dc49
Your Name <you@example.com>
2024-09-04 00:52:55 +0200

    11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+    else:
+        result = 1
+        for _ in range(-b):
+            result *= a
+        result = 1 / result
     return result
