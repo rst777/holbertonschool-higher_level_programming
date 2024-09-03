691ee9302c77fccfa1db7a27cea31dc71ef40fa7
Your Name <you@example.com>
2024-09-03 22:26:32 +0200

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-
-print(", ".join(f"{i:02}" for i in range(100)))
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
