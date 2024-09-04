4d56663cff348d0a06fc0ea94dfe3ba407c1af43
Your Name <you@example.com>
2024-09-04 21:17:10 +0200

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-a = 10
-b = 5
+if __name__ == "__main__":
 
-from calculator_1 import add
-print("{} + {} = {}".format(a, b, add(a,b)))
+    from calculator_1 import add, sub, mul, div
 
-from calculator_1 import sub
-print("{} - {} = {}".format(a, b, sub(a,b)))
-
-from calculator_1 import mul
-print("{} * {} = {}".format(a, b, mul(a,b)))
-
-from calculator_1 import div
-print("{} / {} = {}".format(a, b, div(a,b)))
+    a = 10
+    b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
