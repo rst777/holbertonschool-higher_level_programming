a0287f0c25d48e2cb36e30b7f602f4989add3e0e
Your Name <you@example.com>
2024-09-06 11:04:15 +0200

    101-remove_char_at.py

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
 
-        nouvelle_chaine = ""
-        for i in range(len(str)):
-            if i != n:
-                nouvelle_chaine += str[i]
-        return nouvelle_chaine
+    nouvelle_chaine = ""
+    for i in range(len(str)):
+        if i != n:
+            nouvelle_chaine += str[i]
+    return nouvelle_chaine
