673dfca84f9be197cc3658f1367047e57eb74600
Your Name <you@example.com>
2024-09-09 23:07:39 +0200

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for i, num in enumerate(line):
-            if i < len(line) -1:
+            if i < len(line) - 1:
                 print("{:d}".format(num), end=" ")
             else:
                 print("{:d}".format(num), end="")
         print("$")
-
-
