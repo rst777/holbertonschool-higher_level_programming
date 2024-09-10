b7d364f434a515debd9c6f6eba54e8eaa5465881
Your Name <you@example.com>
2024-09-10 18:42:43 +0200

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+#vérifie si l'entrée est valide
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+# Dictionnaire des valeurs des chiffres romains
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+        }
+    total = 0
+    prev_value = 0
+
+    # Parcours de la chaîne de droite à gauche
+    for char in reversed(roman_string):
+        # Récupère la valeur du caractère romain actuel
+        value = roman_values.get(char, 0)
+
+        # Si la valeur actuelle est inférieure à la valeur précédente,
+        # on la soustrait
+        if value < prev_value:
+            total -= value
+        else:
+            # Sinon, on l'ajoute au total
+            total += value
+
+        # Met à jour la valeur précédente
+        prev_value = value
+    return total
