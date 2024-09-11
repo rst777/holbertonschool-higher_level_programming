4b96f612096669905191c76b2bd88afc8a765da1
Your Name <you@example.com>
2024-09-11 13:34:21 +0200

    python_data

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,12 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print(my_list[i], end='')
+            # Essayer d'accéder et imprimer l'élément courant
+            print(my_list[i], end="")
             count += 1
         except IndexError:
+            # Arrêter la boucle si l'index est hors des limites de la liste
             break
-    print()
+
+    print()  # Passe à la ligne suivante après les éléments imprimés
     return count
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            # Essayer d'accéder et imprimer l'élément courant
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        except IndexError:
+            # Lever une exception si l'index est hors de portée
+            raise IndexError("Index out of range.")
+    print()
+    return count
