febee0fa4760873aad16ad95cabfdf8f75d54d00
Your Name <you@example.com>
2024-09-14 10:30:37 +0200

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,9 +3,8 @@
 This module contains a function that prints a person's name.
 """
 
+
 def say_my_name(first_name, last_name=""):
-
-
     """
     Print "My name is <first_name> <last_name>".
 
@@ -16,11 +15,12 @@ def say_my_name(first_name, last_name=""):
     Raises:
     TypeError: If first_name or last_name is not a string.
     """
-
-
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
