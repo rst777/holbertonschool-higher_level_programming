3c25bf84f65430e8bca486bf6efc34347669ff1b
Your Name <you@example.com>
2024-09-18 14:08:54 +0200

    0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Module that defines a Rectangle class.
+"""
+
+
 class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    Currently, this class doesn't have any attributes or methods.
+    """
     pass
