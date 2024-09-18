3b98863fa6e29966927e8fe744116f43cea2ac5d
Your Name <you@example.com>
2024-09-18 13:54:10 +0200

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
-class Square:
+"""
+Module that defines a Square class.
+"""
+
+
+class Square():
+    """
+    A class to represent a square.
+
+    Attributes:
+        size (int): The size of the square's sides.
+    """
     def __init__(self, size=0):
         self.size = size
 
     @property
     def size(self):
+        """
+        Getter for the size of the square.
+
+        Returns:
+            int: The current size of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+
+        """
+        Setter for the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -16,9 +44,22 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Print the square using the character '#'.
+        If the size is 0, print an empty line.
+
+        Prints:
+            A square of size `size` using '#'.
+        """
         if self.__size == 0:
             print()
         else:
