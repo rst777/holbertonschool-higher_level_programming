dfe4ac9e97a888f2c4665a62eac87977217d8f82
Your Name <you@example.com>
2024-09-18 10:52:45 +0200

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+    __size (int): The size of the square's sides.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square's sides.
+        """
         self.__size__ = size
