e6812eb2fa7b3e7f95c353b7f6a6dfe53fa6d5f5
Your Name <you@example.com>
2024-09-18 10:38:44 +0200

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-
+"""Module for defining a Square class."""
 class Square:
+
+    """
+    Represents a square shape.
+    This is a placeholder class for future implementation.
+    """
+
     pass
