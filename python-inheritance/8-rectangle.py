6cd350995288125f651233d516320544292aa01b
Your Name <you@example.com>
2024-09-24 13:33:28 +0200

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,7 +6,24 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """
+    Une classe représentant un rectangle.
+
+    Cette classe hérite de BaseGeometry et implémente
+    un rectangle avec une largeur et une hauteur.
+    """
     def __init__(self, width, height):
+        """
+        Initialise un nouveau Rectangle.
+
+        Args:
+            width (int): La largeur du rectangle.
+            height (int): La hauteur du rectangle.
+
+        Raises:
+            TypeError: Si width ou height n'est pas un entier.
+            ValueError: Si width ou height est inférieur ou égal à 0.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
