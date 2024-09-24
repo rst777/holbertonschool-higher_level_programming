efe59f359bd8ec5acdb2aacbd3eaf5e9e6b18b0d
Your Name <you@example.com>
2024-09-24 13:52:15 +0200

    5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""Module définissant la classe BaseGeometry."""
+
+
 class BaseGeometry:
+    """
+    Classe de base pour les formes géométriques.
+
+    Cette classe est destinée à être utilisée comme classe parente
+    pour d'autres classes représentant des formes géométriques spécifiques.
+    """
     pass
