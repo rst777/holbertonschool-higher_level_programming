ea78a2c8dbbca9e5492dd3d09659d5aa578812d9
Your Name <you@example.com>
2024-09-25 11:05:59 +0200

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Module définissant la classe BaseGeometry."""
+"""Module définissant la classe BaseGeometry.
+"""
 
 
 class BaseGeometry:
     """Classe de base pour les formes géométriques."""
     def area(self):
-        raise Exception("area() n'est pas implémentée")
+
         """
         Calcule l'aire de la forme géométrique.
 
