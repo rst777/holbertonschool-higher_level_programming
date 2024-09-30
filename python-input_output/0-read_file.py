cc4f8a10f5a3ff67bbbad741c6bedc21f9345fb7
Your Name <you@example.com>
2024-09-30 11:43:56 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""contains the file Read"""
+
 
 def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
     print(content, end="")
