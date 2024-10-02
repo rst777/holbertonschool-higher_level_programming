8b6239891c237b39329aaa38be4613140ccf2c3f
Your Name <you@example.com>
2024-10-02 12:59:10 +0200

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,6 +8,12 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """
+    data:
+        the data to serialize
+    filename:
+        the json file to write in
+    """
     data = {"name": "John Doe", "age": 30, "city": "New York"}
     with open("filename", "w") as file:
         json.dump(data, file)
@@ -15,6 +21,10 @@ def serialize_and_save_to_file(data, filename):
 
 
 def load_and_deserialize(filename):
+    """
+    filename:
+        the json file to load from to create Data
+    """
     with open("filename", "r") as file:
         loaded_data = json.load(file)
     pass
