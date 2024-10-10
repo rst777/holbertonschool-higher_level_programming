2d0550e6c24ac768b9f8da2a21c5d227bf9bdcbc
Your Name <you@example.com>
2024-10-10 12:03:28 +0200

    task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,12 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 
 import json
 import http.server
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
 
         if self.path == '/':
@@ -12,7 +14,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
+            self.wfile.write("Hello, this is a simple API!".encode())
 
         elif self.path == '/data':
             # Réponse JSON pour "/data"
@@ -20,21 +22,22 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write("json.dumps"(data).encode())
+            self.wfile.write(json.dumps(data).encode())
 
         elif self.path == '/status':
             # Réponse pour l'état de l'API
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"OK")
+            self.wfile.write("OK".encode())
 
         else:
             # Gérer les autres chemins
             self.send_response(404)
             self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write("Endpoint not found".encode())
+
 
 if __name__ == "__main__":
     server_address = ('', 8000)
