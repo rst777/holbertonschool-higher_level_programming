3bf70d1f7374629c41fa14b6e47694333eb31075
Your Name <you@example.com>
2024-10-26 12:44:37 +0000

    5-correction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
 -- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT
-    TABLE_NAME AS 'Table',
-    CREATE_TIME AS 'Create Table'
-FROM
-    information_schema.tables
-WHERE
-    table_schema = 'hbtn_0c_0' AND
-    table_name = 'first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table;
