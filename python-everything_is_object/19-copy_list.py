8780fcebb38353067aef83fb4609a570aae991dc
Your Name <you@example.com>
2024-12-04 10:23:36 +0100

    correctif

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def copy_list(a_list):
     return a_list[:]
-# La syntaxe [:] crée une nouvelle liste qui est une copie de a_list.
-# Cela fonctionne pour tous les types d'objets contenus dans la liste.
